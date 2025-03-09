467286b9305fedffefeec243642ecb48b382f79c
kc <toilaluan@github.com>
2025-03-09 15:31:09 +0700

    first commit

---
+++ neuromig-etm/sh_file/run_20NG_100.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define the arrays for weight_GR and weight_InfoNCE
+num_groups_values=(20)
+#weight_GR_values=(1 5 10 20 50)
+# weight_GR_values=(1 5 10)
+weight_GR_values=(5 10 1)
+# weight_InfoNCE_values=(1 10 30 50 80 100 130 150)
+#weight_InfoNCE_values=(80 100 130 150)
+weight_InfoNCE_values=(150 130 100 80)
+
+# Loop through each combination of weight_GR and weight_InfoNCE
+for weight_GR in "${weight_GR_values[@]}"; do
+    for weight_InfoNCE in "${weight_InfoNCE_values[@]}"; do
+        for num_groups in  "${num_groups_values[@]}"; do
+        # Run the python command with the current combination of weight_GR and weight_InfoNCE
+            python main.py --wandb_prj neuromax_20NG_100K --model NeuroMax --dataset 20NG --num_topics 100 --beta_temp 0.2 --num_groups "$num_groups" --epochs 500 --device cuda --lr 0.002 --lr_scheduler StepLR --dropout 0.0 --batch_size 200 --lr_step_size 125 --use_pretrainWE --weight_ECR 40 --weight_GR "$weight_GR" --alpha_ECR 20.0 --alpha_GR 5.0 --weight_InfoNCE "$weight_InfoNCE"
+    done
+done
---
+++ neuromig-fast/main.py
@@ -0,0 +1,226 @@
+from utils import config, log, miscellaneous, seed
+import os
+import numpy as np
+import basic_trainer
+from NeuroMax.NeuroMax import NeuroMax
+import evaluations
+import datasethandler
+import scipy
+import torch
+import wandb
+
+RESULT_DIR = 'results'
+DATA_DIR = 'datasets'
+
+if __name__ == "__main__":
+    parser = config.new_parser()
+    config.add_dataset_argument(parser)
+    config.add_model_argument(parser)
+    config.add_logging_argument(parser)
+    config.add_training_argument(parser)
+    config.add_eval_argument(parser)
+    args = parser.parse_args()
+    
+    prj = args.wandb_prj if args.wandb_prj else 'topmost'
+
+    current_time = miscellaneous.get_current_datetime()
+    current_run_dir = os.path.join(RESULT_DIR, current_time)
+    miscellaneous.create_folder_if_not_exist(current_run_dir)
+
+    config.save_config(args, os.path.join(current_run_dir, 'config.txt'))
+    seed.seedEverything(args.seed)
+    print(args)
+    
+    logger = log.setup_logger(
+        'main', os.path.join(current_run_dir, 'main.log'))
+    wandb.login(key="d00c9f41bdf432ec2cd6df65495965d629331898")
+    wandb.init(project=prj, config=args)
+    wandb.log({'time_stamp': current_time})
+
+    # if args.dataset in ['YahooAnswers']:
+    #     read_labels = True
+    # else:
+    #     read_labels = False
+    read_labels = True
+
+    # load a preprocessed dataset
+    dataset = datasethandler.BasicDatasetHandler(
+        os.path.join(DATA_DIR, args.dataset), device=args.device, read_labels=read_labels,
+        as_tensor=True, contextual_embed=True, plm_model=args.plm_model)
+
+    # create a model
+    pretrainWE = scipy.sparse.load_npz(os.path.join(
+        DATA_DIR, args.dataset, "word_embeddings.npz")).toarray()
+
+    # model = NeuroMax(vocab_size=dataset.vocab_size,
+    #                 num_topics=args.num_topics,
+    #                 num_groups=args.num_groups,
+    #                 dropout=args.dropout,
+    #                 pretrained_WE=pretrainWE if args.use_pretrainWE else None,
+    #                 weight_loss_GR=args.weight_GR,
+    #                 weight_loss_ECR=args.weight_ECR,
+    #                 alpha_ECR=args.alpha_ECR,
+    #                 alpha_GR=args.alpha_GR,
+    #                 weight_loss_InfoNCE=args.weight_InfoNCE,
+    #                 beta_temp=args.beta_temp)
+    model = NeuroMax(vocab_size=dataset.vocab_size, 
+                     num_topics=args.num_topics,
+                    num_groups=args.num_groups, 
+                    hidden_dim_1=dataset.contextual_embed_size, 
+                    hidden_dim_2=dataset.contextual_embed_size, 
+                    embed_size=dataset.contextual_embed_size, 
+                    dropout=args.dropout, 
+                    theta_temp=args.theta_temp,
+                    DT_alpha=args.DT_alpha, TW_alpha=args.TW_alpha,
+                    weight_loss_GR=args.weight_GR,
+                    alpha_GR=args.alpha_GR, 
+                    weight_loss_InfoNCE=args.weight_InfoNCE)
+    model.weight_loss_GR = args.weight_GR
+    model = model.to(args.device)
+
+    # create a trainer
+    trainer = basic_trainer.BasicTrainer(model, epochs=args.epochs,
+                                            learning_rate=args.lr,
+                                            batch_size=args.batch_size,
+                                            lr_scheduler=args.lr_scheduler,
+                                            lr_step_size=args.lr_step_size,
+                                            device=args.device)
+
+
+    # train the model
+    train_simple_embedding, train_theta = trainer.train(dataset)
+
+    # save beta, theta and top words
+    beta = trainer.save_beta(current_run_dir)
+    test_theta = trainer.save_theta(train_theta, dataset, current_run_dir)
+    #test_theta = trainer.model.get_theta(dataset.test_contextual_embed, train_simple_embedding)
+    #test_theta = np.asarray(test_theta.cpu())
+    top_words_10 = trainer.save_top_words(
+        dataset.vocab, 10, current_run_dir)
+    top_words_15 = trainer.save_top_words(
+        dataset.vocab, 15, current_run_dir)
+    top_words_20 = trainer.save_top_words(
+        dataset.vocab, 20, current_run_dir)
+    top_words_25 = trainer.save_top_words(
+        dataset.vocab, 25, current_run_dir)
+
+    # argmax of train and test theta
+    # train_theta_argmax = train_theta.argmax(axis=1)
+    # test_theta_argmax = test_theta.argmax(axis=1) 
+    train_theta = np.asarray(train_theta.cpu())
+    train_theta_argmax = train_theta.argmax(axis=1)
+    unique_elements, counts = np.unique(train_theta_argmax, return_counts=True)
+    print(f'train theta argmax: {unique_elements, counts}')
+    logger.info(f'train theta argmax: {unique_elements, counts}')
+    test_theta_argmax = test_theta.argmax(axis=1)
+    unique_elements, counts = np.unique(test_theta_argmax, return_counts=True)
+    print(f'test theta argmax: {unique_elements, counts}')
+    logger.info(f'test theta argmax: {unique_elements, counts}')       
+
+    # TD_15 = evaluations.compute_topic_diversity(
+    #     top_words_15, _type="TD")
+    # print(f"TD_15: {TD_15:.5f}")
+
+
+    # # evaluating clustering
+    # if read_labels:
+    #     clustering_results = evaluations.evaluate_clustering(
+    #         test_theta, dataset.test_labels)
+    #     print(f"NMI: ", clustering_results['NMI'])
+    #     print(f'Purity: ', clustering_results['Purity'])
+
+
+    # TC_15_list, TC_15 = evaluations.topic_coherence.TC_on_wikipedia(
+    #     os.path.join(current_run_dir, 'top_words_15.txt'))
+    # print(f"TC_15: {TC_15:.5f}")
+    TD_10 = evaluations.compute_topic_diversity(
+        top_words_10, _type="TD")
+    print(f"TD_10: {TD_10:.5f}")
+    wandb.log({"TD_10": TD_10})
+    logger.info(f"TD_10: {TD_10:.5f}")
+
+    TD_15 = evaluations.compute_topic_diversity(
+        top_words_15, _type="TD")
+    print(f"TD_15: {TD_15:.5f}")
+    wandb.log({"TD_15": TD_15})
+    logger.info(f"TD_15: {TD_15:.5f}")
+
+    # TD_20 = topmost.evaluations.compute_topic_diversity(
+    #     top_words_20, _type="TD")
+    # print(f"TD_20: {TD_20:.5f}")
+    # wandb.log({"TD_20": TD_20})
+    # logger.info(f"TD_20: {TD_20:.5f}")
+
+    # TD_25 = topmost.evaluations.compute_topic_diversity(
+    #     top_words_25, _type="TD")
+    # print(f"TD_25: {TD_25:.5f}")
+    # wandb.log({"TD_25": TD_25})
+    # logger.info(f"TD_25: {TD_25:.5f}")
+
+    # evaluating clustering
+    if read_labels:
+        clustering_results = evaluations.evaluate_clustering(
+            test_theta, dataset.test_labels)
+        print(f"NMI: ", clustering_results['NMI'])
+        print(f'Purity: ', clustering_results['Purity'])
+        wandb.log({"NMI": clustering_results['NMI']})
+        wandb.log({"Purity": clustering_results['Purity']})
+        logger.info(f"NMI: {clustering_results['NMI']}")
+        logger.info(f"Purity: {clustering_results['Purity']}")
+
+    # evaluate classification
+    if read_labels:
+        classification_results = evaluations.evaluate_classification(
+            train_theta, test_theta, dataset.train_labels, dataset.test_labels, tune=args.tune_SVM)
+        print(f"Accuracy: ", classification_results['acc'])
+        wandb.log({"Accuracy": classification_results['acc']})
+        logger.info(f"Accuracy: {classification_results['acc']}")
+        print(f"Macro-f1", classification_results['macro-F1'])
+        wandb.log({"Macro-f1": classification_results['macro-F1']})
+        logger.info(f"Macro-f1: {classification_results['macro-F1']}")
+
+    #TC
+    TC_15_list, TC_15 = evaluations.topic_coherence.TC_on_wikipedia(
+        os.path.join(current_run_dir, 'top_words_15.txt'))
+    print(f"TC_15: {TC_15:.5f}")
+    wandb.log({"TC_15": TC_15})
+    logger.info(f"TC_15: {TC_15:.5f}")
+    logger.info(f'TC_15 list: {TC_15_list}')
+
+    # TC_10_list, TC_10 = topmost.evaluations.topic_coherence.TC_on_wikipedia(
+    #     os.path.join(current_run_dir, 'top_words_10.txt'))
+    # print(f"TC_10: {TC_10:.5f}")
+    # wandb.log({"TC_10": TC_10})
+    # logger.info(f"TC_10: {TC_10:.5f}")
+    # logger.info(f'TC_10 list: {TC_10_list}')
+
+    # NPMI
+    # NPMI_train_10_list, NPMI_train_10 = evaluations.compute_topic_coherence(
+    #     dataset.train_texts, dataset.vocab, top_words_10, cv_type='c_npmi')
+    # print(f"NPMI_train_10: {NPMI_train_10:.5f}, NPMI_train_10_list: {NPMI_train_10_list}")
+    # wandb.log({"NPMI_train_10": NPMI_train_10})
+    # logger.info(f"NPMI_train_10: {NPMI_train_10:.5f}")
+    # logger.info(f'NPMI_train_10 list: {NPMI_train_10_list}')
+
+    # NPMI_wiki_10_list, NPMI_wiki_10 = evaluations.topic_coherence.TC_on_wikipedia(
+    #     os.path.join(current_run_dir, 'top_words_10.txt'), cv_type='NPMI')
+    # print(f"NPMI_wiki_10: {NPMI_wiki_10:.5f}, NPMI_wiki_10_list: {NPMI_wiki_10_list}")
+    # wandb.log({"NPMI_wiki_10": NPMI_wiki_10})
+    # logger.info(f"NPMI_wiki_10: {NPMI_wiki_10:.5f}")
+    # logger.info(f'NPMI_wiki_10 list: {NPMI_wiki_10_list}')
+
+    # Cp_wiki_10_list, Cp_wiki_10 = evaluations.topic_coherence.TC_on_wikipedia(
+    #     os.path.join(current_run_dir, 'top_words_10.txt'), cv_type='C_P')
+    # print(f"Cp_wiki_10: {Cp_wiki_10:.5f}, Cp_wiki_10_list: {Cp_wiki_10_list}")
+    # wandb.log({"Cp_wiki_10": Cp_wiki_10})
+    # logger.info(f"Cp_wiki_10: {Cp_wiki_10:.5f}")
+    # logger.info(f'Cp_wiki_10 list: {Cp_wiki_10_list}')
+    
+    # w2v_list, w2v = topmost.evaluations.topic_coherence.compute_topic_coherence(
+    #     dataset.train_texts, dataset.vocab, top_words_10, cv_type='c_w2v')
+    # print(f"w2v: {w2v:.5f}, w2v_list: {w2v_list}")
+    # wandb.log({"w2v": w2v})
+    # logger.info(f"w2v: {w2v:.5f}")
+    # logger.info(f'w2v list: {w2v_list}')
+
+    wandb.finish()
